4b1a4ff3a7500ba920ff14165d1042461bec2634
Chris Uzelac <cuzelac@gmail.com>
2012-03-29 19:16:48 -0700

    use validates instead of validates_*_of

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,14 +1,13 @@
 class Link < ActiveRecord::Base
   attr_accessible :shortcut, :target, :access_count
 
-  validates :access_count, :presence => true, :default => 0
-  #validates :access_count, :presence => true, :default => 0
-
+  validates :access_count, :presence => { :default => 0 }
   validates :shortcut,
-            :presence => true,
-            :format => {
-              :with => /^[a-z\d]+(?:-[a-z\d]+)*[a-z\d]*$/,
-              :message => "Only lower-case letters and numbers, with a dash as a word separator",
-            }
+    :presence => true,
+    :uniqueness => true,
+    :format => {
+      :with => /^[a-z\d]+(?:-[a-z\d]+)*[a-z\d]*$/,
+      :message => "Only lower-case letters and numbers, with a dash as a word separator",
+    }
   validates :target, :presence => true
 end
